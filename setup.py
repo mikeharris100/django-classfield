865200b28371c58839db9c6129681b2a20cb6f1a
MikeAmy <cocoade@googlemail.com>
2014-10-13 18:49:45 +0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-classfield',
+    version='1.0',
+    description='Adds a class field to django',
+    author='Mike Harris',
+    url='https://github.com/mikeharris100/django-classfield',
+    py_modules=['classfield']
+)
